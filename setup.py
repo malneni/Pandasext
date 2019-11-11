ef9c2526c41e3940b27f02a8f67181918cec4edb
Malneni Venkatesh <malneni258@gmail.com>
2019-11-11 16:40:45 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='pdext',
+      version='0.0.1',
+      description='Pandas Extention python module',
+      long_description=readme(),
+      url='https://github.com/malneni/PdExt',
+      author='Venkatesh Malneni',
+      author_email='malneni258@gmail.com',
+      license='MIT',
+      install_requires=['pandas', 'docx', 'numpy'],
+      packages=['pdext'],
+      classifiers=["Programming Language :: Python :: 3.7"],
+      zip_safe=False,
+      )
